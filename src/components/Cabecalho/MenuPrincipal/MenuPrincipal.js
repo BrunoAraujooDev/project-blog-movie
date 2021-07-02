8a84ce29a633e7db2d274eb6c40d7b149939e48d
Bruno Araujo <brunoaraujo2395@gmail.com>
2021-07-01 21:16:45 -0300

    Ajustando o css

--- src/components/Cabecalho/MenuPrincipal/MenuPrincipal.js
+++ src/components/Cabecalho/MenuPrincipal/MenuPrincipal.js
@@ -29,9 +29,9 @@ const MenuPrincipal = () => {
     @media screen and (max-width: 720px) {
         
         display: ${({display}) => display ? display : "flex"  };
+
     }
-            
-        
+              
     `
     
 
@@ -47,28 +47,28 @@ const MenuPrincipal = () => {
         
         <TemaNav id="nav-menu-principal"  display={menuAberto ? "none" : ""} >
         <ul className="ul-lista-menu-principal" style={ { backgroundColor: tema.corFundoTema} }>
-                <TemaLi><Link className="lista-menu-principal" to="/lista-filmes">Filmes</Link></TemaLi>
-                <TemaLi ><Link className="lista-menu-principal" to="/lista-review">Reviews</Link></TemaLi>
-                <TemaLi  className="lista-menu-principal">
-                <Link  className="lista-menu-categoria" to="/lista-categoria">Categorias</Link>
+            <TemaLi><Link className="lista-menu-principal" to="/lista-filmes">Filmes</Link></TemaLi>
+            <TemaLi ><Link className="lista-menu-principal" to="/lista-review">Reviews</Link></TemaLi>
+            <TemaLi  className="lista-menu-principal">
+            <Link  className="lista-menu-categoria" to="/lista-categoria">Categorias</Link>
 
-                    <ul className="div-lista-categoria" style={ { backgroundColor: tema.corFundoTema} }>
-                        
-                        {categorias.map( item => {
-                            return <TemaLi key={item.id}  ><Link className="lista-menu-principal"to={`/review-por-categoria/${item.id}`}>{item.descricao}</Link></TemaLi>
-                        })}
+                <ul className="div-lista-categoria" style={ { backgroundColor: tema.corFundoTema} }>
+                    
+                    {categorias.map( item => {
+                        return <TemaLi key={item.id}  ><Link className="lista-menu-principal"to={`/review-por-categoria/${item.id}`}>{item.descricao}</Link></TemaLi>
+                    })}
 
-                    </ul>
-                </TemaLi>
-                <TemaLi><Link className="lista-menu-principal" to="/lista-novo-review">Novo review</Link></TemaLi>
-            </ul>
-            </TemaNav>  
+                </ul>
+            </TemaLi>
+            <TemaLi><Link className="lista-menu-principal" to="/lista-novo-review">Novo review</Link></TemaLi>
+        </ul>
+        </TemaNav>  
         
              
          <div id ="div-cabecalho-hamburguer" style={ { backgroundColor: tema.corTemaEscuro} }  onClick={() => setMenuAberto(!menuAberto)}>
-         <div style={ { backgroundColor: tema.corFundoTema} }></div>
-         <div style={ { backgroundColor: tema.corFundoTema} }></div>
-         <div style={ { backgroundColor: tema.corFundoTema} }></div>
+            <div style={ { backgroundColor: tema.corFundoTema} }></div>
+            <div style={ { backgroundColor: tema.corFundoTema} }></div>
+            <div style={ { backgroundColor: tema.corFundoTema} }></div>
          </div>
         </>
     );
